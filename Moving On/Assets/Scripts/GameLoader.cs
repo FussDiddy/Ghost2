bc99f6f55579d1e69f31d0003481f99bd7f1ee5f
Max O <56001077+TheRatMan1@users.noreply.github.com>
2022-09-28 17:50:30 -0700

    Proper Scene Changes Added
    -Created functioning play button that transitions to initial gameplay scene
    
    -Fixed code issue causing the play to keep transitioning scenes by clicking anywhere on screen

--- Moving On/Assets/Scripts/GameLoader.cs
+++ Moving On/Assets/Scripts/GameLoader.cs
@@ -5,14 +5,21 @@ using UnityEngine.SceneManagement;
 public class GameLoader : MonoBehaviour
 {
     public Animator transition;
-
     // Update is called once per frame
     void Update()
     {
-       if(Input.anyKey)
-        {
-            LoadLevel();
+        //Find what the name of the currently loaded scene
+        Scene activeScene = SceneManager.GetActiveScene();
 
+        string activeName = activeScene.name;
+        //If the splash screen is loaded, transistion to next scene when any button is pressed
+        //Otherwise, do not transistion scenes
+        if (activeName == "Splash Screen")
+        {
+            if (Input.anyKey)
+            {
+                LoadLevel();
+            }
         }
     }
 
---
+++ Moving On/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public Animator transition;
+    public void PlayButtonScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play the animation
+        transition.SetTrigger("Start");
+        //Wait
+        yield return new WaitForSeconds(1);
+        //Then the load the scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
