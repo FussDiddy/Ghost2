8ef9f8e7009187bdf65ee00f4889c79816251d98
Max O <56001077+TheRatMan1@users.noreply.github.com>
2022-09-28 20:18:29 -0700

    Bug Fixes

--- Moving On/Assets/Scripts/CorrectAnswer.cs
+++ Moving On/Assets/Scripts/CorrectAnswer.cs
@@ -5,19 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class CorrectAnswer : MonoBehaviour
 {
-    public Animator transition;
     public void RightAnswer()
     {
         SceneManager.LoadScene(3);
-    }
-
-    IEnumerator LoadLevel(int levelIndex)
-    {
-        //Play the animation
-        transition.SetTrigger("Start");
-        //Wait
-        yield return new WaitForSeconds(1);
-        //Then the load the scene
-        SceneManager.LoadScene(levelIndex);
     }
 }
--- Moving On/Assets/Scripts/IncorrectAnswer.cs
+++ Moving On/Assets/Scripts/IncorrectAnswer.cs
@@ -5,19 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class IncorrectAnswer : MonoBehaviour
 {
-    public Animator transition;
     public void WrongAnswer()
     {
         SceneManager.LoadScene(4);
     }
 
-    IEnumerator LoadLevel(int levelIndex)
-    {
-        //Play the animation
-        transition.SetTrigger("Start");
-        //Wait
-        yield return new WaitForSeconds(1);
-        //Then the load the scene
-        SceneManager.LoadScene(levelIndex);
-    }
 }
