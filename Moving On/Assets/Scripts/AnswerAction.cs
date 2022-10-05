242cd6eed52b2a757e86e868cd5152e5c7aa5c44
FussDiddy <justin@justinfuss.com>
2022-10-05 00:52:22 -0400

    Player score functionality added

--- Moving On/Assets/Scripts/AnswerAction.cs
+++ Moving On/Assets/Scripts/AnswerAction.cs
@@ -6,9 +6,18 @@ public class AnswerAction : MonoBehaviour
 {
     public GameObject currentPanel;
     public GameObject targetPanel;
+    public bool answerCorrect;
+    public int questionWorth;
 
     public void ClickAction()
     {
+        if(answerCorrect == true)
+        {
+            //Update score
+            ScoreManager.instance.AddPoints(questionWorth);
+        }
+
+        //Move to correct response text
         currentPanel.SetActive(false);
         targetPanel.SetActive(true);
     }
