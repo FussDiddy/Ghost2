74017b533699abe32726bdd1e3d90d2948d734df
Max O <56001077+TheRatMan1@users.noreply.github.com>
2022-11-02 18:45:39 -0700

    Added Pause Menu + Volume Slider

---
+++ Moving On/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public Animator transition;
+
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            } 
+            else
+            {
+                Pause();
+            }
+        }
+    } 
+
+  public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("TempSceneChange");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
